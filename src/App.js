0779c1b6344f2db961de4a85ff1af973783d8672
Amos Gichero <amosgichero@gmail.com>
2020-02-01 18:31:45 -0500

    navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Home} from './components/Home/Home';
-import {About} from './components/About/About';
-import {Contact} from './components/Contact/Contact';
-import './App.css';
+import {Home} from './containers/Home/Home';
+import {About} from './containers/About/About';
+import {Contact} from './containers/Contact/Contact';
+import {Layout} from './components/Layout/Layout';
+import {NavigationBar} from './components/NavigationBar/NavigationBar';
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/contact" component={Contact}/>
-        </Switch>
-      </Router>
+      <NavigationBar/>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
+        </Router>
+      </Layout>
+      
     </React.Fragment>
   );
 }
