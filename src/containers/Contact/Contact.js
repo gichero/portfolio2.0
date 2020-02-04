841a136a93f22fed6bcf69e756023e1902ee1cf2
Amos Gichero <amosgichero@gmail.com>
2020-02-04 15:29:52 -0500

    styling added

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+#contact{
+    text-align: center;
+}
+#intro{
+    text-align: center;
+    line-height: 2;  
+}
+`;
 
 export const Contact = () => (
-    <div>
-        <h2>Contact Page</h2>
-        <p>
-        I'd love to connect with you! I can be reached via email or any social media outlets. You can also check out my GitHub account to see more of my work, or download my resume to get information about my professional background.
+    <Styles>
+        <h2 id="contact">Contact</h2>
+        <p id="intro">
+        I'd look forward to connect with you! I can be reached via email or any social media outlets. You can also check out my GitHub account to see more of my work, or download my resume to get information about my professional background.
         </p>
-        
-    </div>
+    </Styles>
 )
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    #name{
+        text-align: left;
+    }
+    #intro{
+        text-align: left;
+        line-height: 2;  
+    }
+`;
 
 export const Home = () => (
 
-    <div>
-        <h2>Hi, I'm Amos Gichero</h2>
-        <p>
+    <Styles>
+        <h2 id="name">Hi, I'm Amos Gichero</h2>
+        <p id="intro">
         I'm a Full Stack Web Developer living in Atlanta, GA. 
         I'm originally from New Orleans and attended the University of Georgia
         for my undergraduate degree where I majored in Psychology. 
@@ -14,7 +25,9 @@ export const Home = () => (
         I spent a year teaching myself coding through online tutorials (and a lot of trial and error!)
         and building websites and games for myself and friends. I realized I loved programming and wanted to pursue web development as a career. 
         I graduated from DigitalCrafts' four month, full-time immersive coding bootcamp in June of 2017.
-        </p>  
-    </div>
+        </p>
+        <img/>
+        <img/>  
+    </Styles>
     
 )
