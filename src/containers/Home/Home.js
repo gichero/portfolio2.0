f8c9265292020dcb8e325112cf348d11610544df
Amos Gichero <amosgichero@gmail.com>
2020-02-10 09:53:18 -0500

    intro complete, add styling

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -18,17 +18,17 @@ const Styles = styled.div`
 export const Home = () => (
 
     <Styles>
-        <h2 id="name">Hi, I'm Amos Gichero</h2>
+        <h3 id="name">Hi, I'm Amos Gichero</h3>
         <p id="intro">
         I'm a Full Stack Web Developer living in Jacksonville, FL. 
-        I'm originally from Atlanta by way of Australia where I attended the University of Canberra
+        I'm originally from Atlanta by way of Australia. I attended the University of Canberra
         for my undergraduate degree where I majored in Information Systems and Business. 
-        Upon graduation I moved to Nairobi, Kenya and began working on Capitol Hill for David Vitter, a US Senator from Louisiana. 
-        After many years in the political realm I decided I was ready for a new challenge. 
+        I moved to the United States in 2009 and worked as a truck driver before I decided I was ready for a new challenge. 
         I've always loved science and technology so web development was a perfect fit for my next endeavor.
-        I spent a year teaching myself coding through online tutorials (and a lot of trial and error!)
+        I enrolled and completed a computer science program at Chattahoochee technical college all along while
+        teaching myself coding through online tutorials (and a lot of trial and error!)
         and building websites and games for myself and friends. I realized I loved programming and wanted to pursue web development as a career. 
-        I graduated from DigitalCrafts' four month, full-time immersive coding bootcamp in June of 2017.
+        I graduated from DigitalCrafts' a four month, full-time immersive coding bootcamp in June of 2017.
         </p>
         <img/>
         <img/>  
